3291441bc076ab086d6faf33e321f0d702618cc9
hahs <alexandre_2420@outlook.com>
2022-08-27 15:43:32 -0500

    add items. dinamics forms

--- src/app/template/dinamics/dinamics.component.ts
+++ src/app/template/dinamics/dinamics.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  name: string;
+  favorites: Fav[];
+}
+
+interface Fav {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styleUrls: ['./dinamics.component.css']
+  styleUrls: ['./dinamics.component.css'],
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
+  person: Person = {
+    name: 'Alex',
+    favorites: [
+      {
+        id: 1,
+        name: 'The last of us',
+      },
+      {
+        id: 2,
+        name: 'Uncharted',
+      },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  save() {
+    console.log('sending..');
   }
-
 }
