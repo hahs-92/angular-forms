67d478ab2620827213f978282571eccc3837ee38
hahs <alexandre_2420@outlook.com>
2022-09-03 08:50:45 -0500

    email validation

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -8,9 +8,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent {
   patternName = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.patternName)]],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailPattern),
+      ],
+    ],
   });
 
   constructor(private fb: FormBuilder) {}
