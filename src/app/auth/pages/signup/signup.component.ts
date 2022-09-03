21102a7fc76f367fb74704537fc01946dece66b8
hahs <alexandre_2420@outlook.com>
2022-09-03 09:07:47 -0500

    custom validaions

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +14,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignupComponent {
   patternName = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+  striderNotAllow(control: FormControl) {
+    const value = control?.value;
+
+    if (value === 'strider') {
+      return { noStrider: true }; // con que devolvamos un obj estamos diciendo que es un error
+    }
+
+    return null;
+  }
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.patternName)]],
@@ -20,6 +35,7 @@ export class SignupComponent {
         Validators.pattern(this.emailPattern),
       ],
     ],
+    username: ['', [Validators.required, this.striderNotAllow]],
   });
 
   constructor(private fb: FormBuilder) {}
